b7b1d00ee0e29ba70da6ddcd1f62b77271811c99
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2023-02-08 22:21:08 -0300

    login google

--- App.tsx
+++ App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import { ThemeProvider } from 'styled-components';
-import theme from './src/global/styles/theme';
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react'
+import * as SplashScreen from 'expo-splash-screen'
+import { ThemeProvider } from 'styled-components'
+import theme from './src/global/styles/theme'
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold
-} from '@expo-google-fonts/poppins';
+} from '@expo-google-fonts/poppins'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { AppRoutes } from './src/routes/app.routes';
+import { NavigationContainer } from '@react-navigation/native'
+import { AppRoutes } from './src/routes/app.routes'
+import { SignIn } from './src/screens/SignIn/index'
 
-
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,16 +24,16 @@ export default function App() {
     Poppins_500Medium,
     Poppins_700Bold
   })
-  if(!fontsLoaded) return null;
-  
-  SplashScreen.hideAsync();
+  if (!fontsLoaded) return null
+
+  SplashScreen.hideAsync()
 
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <AppRoutes />
+        {/* <AppRoutes /> */}
+        <SignIn />
       </NavigationContainer>
-    </ThemeProvider>    
-  );
+    </ThemeProvider>
+  )
 }
-
--- src/components/LoadingAnimation/index.tsx
+++ src/components/LoadingAnimation/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import LottieView from 'lottie-react-native';
+import React from 'react'
+import LottieView from 'lottie-react-native'
 
-import { Container } from './styles';
+import { Container } from './styles'
 
-export function LoadingAnimation(){
-    return (
-        <Container>
-            <LottieView 
-                source={require('../../assets/pie-chart.json')}
-                style={{height: 300}}                
-                resizeMode="contain"
-                loop
-                autoPlay
-            />
-        </Container>
-    )
+export function LoadingAnimation() {
+  return (
+    <Container>
+      <LottieView
+        source={require('../../assets/spinning-pie-chart.json')}
+        style={{ height: 300 }}
+        resizeMode="contain"
+        loop
+        autoPlay
+      />
+    </Container>
+  )
 }
